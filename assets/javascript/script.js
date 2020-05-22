91b2d248d9d97a9a464ed46a93d0313fe575a268
SarahRubio <sarahrubio.pro@gmail.com>
2020-05-22 14:15:43 +0200

    refactorisation fonction avec dataset

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -37,35 +37,26 @@ animated_text1()
 // Affichage path (refactorisation)
 
 function RendreVisible(classPath) {
-  let l = `${classPath}line`;
-  let t = `${classPath}text`;
-  let tb = `${classPath}textbis`;
-  let a = `${classPath}arrow` ;
-  document.querySelector(l).style.visibility="visible";
-  document.querySelector(t).style.visibility="visible";
-  document.querySelector(tb).style.visibility="visible";
-  document.querySelector(a).style.visibility="visible";
-}
+  document.querySelectorAll('[data-color="' + classPath + '"]').forEach(function(path){
+    path.style.visibility="visible";
+  });
+};
 
 function RendreInvisible(classPath) {
-  let l = `${classPath}line`;
-  let t = `${classPath}text`;
-  let tb = `${classPath}textbis`;
-  let a = `${classPath}arrow` ;
-  document.querySelector(l).style.visibility="hidden";
-  document.querySelector(t).style.visibility="hidden";
-  document.querySelector(tb).style.visibility="hidden";
-  document.querySelector(a).style.visibility="hidden";
-}
+  document.querySelectorAll('[data-color="' + classPath + '"]').forEach(function(path){
+    path.style.visibility="hidden";
+  });
+};
+
 if(document.querySelector('.red')) {
 document.querySelector('.red').addEventListener("mouseenter", function () {
-  RendreVisible('.red')
+  RendreVisible('red');
 });
 }
 
 if(document.querySelector('.red')) {
 document.querySelector('.red').addEventListener("mouseout", function () {
-  RendreInvisible('.red')
+  RendreInvisible('red')
 });
 }
 
@@ -170,81 +161,3 @@ document.querySelector('.red').addEventListener("mouseout", function () {
 //   startTrombi1();
 // })
 // }
-
-
-// ///////////////////////////////////////////// REMAIN EN ATTENTE DE refactorisation
-//
-//
-// document.querySelector(".red").addEventListener("mouseenter", function () {
-//   document.querySelector(".redline").style.visibility="visible";
-//   document.querySelector(".redtext").style.visibility="visible";
-//   document.querySelector(".redtextbis").style.visibility="visible";
-//   document.querySelector(".redarrow").style.visibility="visible";
-// });
-//
-// document.querySelector(".red").addEventListener("mouseout", function () {
-//   document.querySelector(".redline").style.visibility="hidden";
-//   document.querySelector(".redtext").style.visibility="hidden";
-//   document.querySelector(".redtextbis").style.visibility="hidden";
-//   document.querySelector(".redarrow").style.visibility="hidden";
-// });
-//
-// document.querySelector(".yellow").addEventListener("mouseenter", function () {
-//   document.querySelector(".yellowtext").style.visibility="visible";
-//   document.querySelector(".yellowline").style.visibility="visible";
-//   document.querySelector(".yellowarrow").style.visibility="visible";
-// });
-//
-// document.querySelector(".yellow2").addEventListener("mouseout", function () {
-//   document.querySelector(".yellowtext2").style.visibility="hidden";
-//   document.querySelector(".yellowline2").style.visibility="hidden";
-//   document.querySelector(".yellowarrow2").style.visibility="hidden";
-// });
-//
-// document.querySelector(".yellow2").addEventListener("mouseenter", function () {
-//   document.querySelector(".yellowtext2").style.visibility="visible";
-//   document.querySelector(".yellowline2").style.visibility="visible";
-//   document.querySelector(".yellowarrow2").style.visibility="visible";
-// });
-//
-// document.querySelector(".yellow").addEventListener("mouseout", function () {
-//   document.querySelector(".yellowtext").style.visibility="hidden";
-//   document.querySelector(".yellowline").style.visibility="hidden";
-//   document.querySelector(".yellowarrow").style.visibility="hidden";
-// });
-//
-// document.querySelector(".red2").addEventListener("mouseenter", function () {
-//   document.querySelector(".redtext2").style.visibility="visible";
-//   document.querySelector(".redline2").style.visibility="visible";
-//   document.querySelector(".redarrow2").style.visibility="visible";
-// });
-//
-// document.querySelector(".red2").addEventListener("mouseout", function () {
-//   document.querySelector(".redtext2").style.visibility="hidden";
-//   document.querySelector(".redline2").style.visibility="hidden";
-//   document.querySelector(".redarrow2").style.visibility="hidden";
-// });
-//
-// document.querySelector(".green").addEventListener("mouseenter", function () {
-//   document.querySelector(".greentext").style.visibility="visible";
-//   document.querySelector(".greenline").style.visibility="visible";
-//   document.querySelector(".greenarrow").style.visibility="visible";
-// });
-//
-// document.querySelector(".green").addEventListener("mouseout", function () {
-//   document.querySelector(".greentext").style.visibility="hidden";
-//   document.querySelector(".greenline").style.visibility="hidden";
-//   document.querySelector(".greenarrow").style.visibility="hidden";
-// });
-//
-// document.querySelector(".red3").addEventListener("mouseenter", function () {
-//   document.querySelector(".redtext3").style.visibility="visible";
-//   document.querySelector(".redline3").style.visibility="visible";
-//   document.querySelector(".redarrow3").style.visibility="visible";
-// });
-//
-// document.querySelector(".red3").addEventListener("mouseout", function () {
-//   document.querySelector(".redtext3").style.visibility="hidden";
-//   document.querySelector(".redline3").style.visibility="hidden";
-//   document.querySelector(".redarrow3").style.visibility="hidden";
-// });
